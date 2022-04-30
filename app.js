dfe220d675a3e0f1241b9151c5462f0b957c6710
Ojas Karmarkar <ojask2002@gmail.com>
2022-04-30 09:00:27 +0530

    connect to db , init done

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const express = require('express')
+import dotenv from 'dotenv'
+import express from 'express'
+import db from './db/db.js'
+import morgan from 'morgan'
+// import userRouter from './routes/user.js'
+// import activityRouter from './routes/activity.js'
+import cors from 'cors'
+
+const port = process.env.PORT || 3001
+
+dotenv.config()
 const app = express()
-const port = 8000
+await db()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(morgan(':method :url :status :response-time ms'))
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use(cors())
+app.use(express.json())
+
+// app.use('/api/user', userRouter)
+// app.use('/api/activity', activityRouter)
+
+const server = app.listen(port, () =>
+  console.log(`server has started on port ${port}`)
+)
+
+export default server
